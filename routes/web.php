c28afb5f38e1ddc679f22bf5268fb8bf46d6e354
MichaelAgustinn <michaelagustinn080@gmail.com>
2025-05-11 21:22:55 +0800

    [style: add template]

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,35 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('anu');
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
+
+    Route::get('/editor', function () {
+        return view('dashboard.editor');
+    })->name('editor');
+
+    Route::get('/general', [LandingPageController::class, 'index'])->name('general');
+    Route::post('/general/store', [LandingPageController::class, 'tambah'])->name('generalStore');
+
+    Route::get('/listuser', [UserController::class, 'index'])->name('listuser');
+    Route::get('/validate-member', [UserController::class, 'validate'])->name('validate');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +37,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/testing', function () {
+    return 'MASUK MASS';
+})->name('testing');
+
+// Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('dashboard');
+require __DIR__ . '/auth.php';
